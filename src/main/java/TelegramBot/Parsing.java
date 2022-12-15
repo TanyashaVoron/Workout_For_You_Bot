1a45e144085ce36d2e48419deafce921c8c33345
TanyashaVoron <82428036+TanyashaVoron@users.noreply.github.com>
2022-12-15 15:44:43 +0500

    исправлены неполадки

--- src/main/java/TelegramBot/Parsing.java
+++ src/main/java/TelegramBot/Parsing.java
@@ -11,7 +11,7 @@ public class Parsing
 {
     public String getVideoLink(String request) throws IOException
     {
-        String url = "https://www.youtube.com/results?search_query="+ URLEncoder.encode(request, StandardCharsets.UTF_8);
+        String url = "https://www.youtube.com/results?search_query=workout"+ URLEncoder.encode(request, StandardCharsets.UTF_8);
         String page = String.valueOf(Jsoup.parse(new URL(url),3000));
         int count=page.indexOf("\"url\":\"/watch?v=")+16;
         StringBuilder linkToVideo = new StringBuilder("https://www.youtube.com/watch?v=");
